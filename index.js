018dfa03089ddd1596b39d75e24330116249542f
arturs <arturs.kirtovskis@gmail.com>
2020-11-01 21:04:14 +0200

    add better log messages

--- index.js
+++ index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 
+const testLogs = () => {
+  setInterval(()=> console.log(`Log 10 sec before listening LOG`),20000);
+  setInterval(()=> console.log(`Listening on: ${ PORT }`),30000);
+  setInterval(()=> console.log(`Log 10 sec after listening LOG`),40000);
+}
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT,  setInterval(()=>console.log(`Listening on ${ PORT }`),5000))
+  .listen(PORT,  testLogs())
