baebaf605354f5c8fcb392e17da54bc3c4f77732
Arturs Kirtovskis <arturs.kirtovskis@gmail.com>
2021-03-02 18:48:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 const testLogs = () => {
-  setInterval(()=> console.log(`Redeploying with NEWEST approachyy`),20000);
+  setInterval(()=> console.log(`REDEPLOYED with NEWEST approachyy`),20000);
   setInterval(()=> console.log(`Listening on: ${ PORT }`),30000);
 }
 
